c6a40df286d8cc27460854f77025a478209e9f57
Stéphen Le Maistre <slemaistre@linagora.com>
2013-10-04 11:02:13 +0200

    OBMFULL-5433 remove useless files and squash the proxy into app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+'use strict';
 
-/**
- * Module dependencies.
- */
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
 
-var config = require('./lib/config');
-var logger = require('./lib/logger');
-var express = require('express');
-var routes = require('./routes');
-var proxy = require('./routes/updates');
-var http = require('http');
-var path = require('path');
+var proxy = require('./routes/updates'),
+    config = require('./lib/config'),
+    logger = require('./lib/logger');
 
 var app = express();
 
@@ -28,6 +25,10 @@ app.use('/download', express.static(config.download.dir));
 
 logger.info('Dumping server configuration :', config);
 app.set('port', config.server.port);
+
+app.put('/admin/updates/:id/update/:indexOfUpdate', admin.activateUpdate);
+app.delete('/admin/updates/:id/update/:indexOfUpdate', admin.deactivateUpdate);
+
 http.createServer(app).listen(app.get('port'), function() {
   logger.info('mozilla-updater server listening on port %d', app.get('port'));
 });
