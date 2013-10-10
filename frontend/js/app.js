5aad77a95a96b59292f0897b78c3f81adb884f98
dready92 <mickael.bailly@free.fr>
2013-10-11 00:20:21 +0200

    [OBMFULL-5454] use mupeeProduct module

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,8 +2,10 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngRoute']).
+angular.module('myApp', ['ngRoute', 'mupeeProduct']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home'});
+    $routeProvider.when('/Firefox', {templateUrl: 'partials/productHome', controller: 'productHome'});
+    $routeProvider.when('/Thunderbird', {templateUrl: 'partials/productHome', controller: 'productHome'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
