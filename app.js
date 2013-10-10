c40ce890055e354e8b36783ad5f3efd441411e36
Michael Bailly <mbailly@linagora.com>
2013-10-10 12:21:33 +0200

    [OBMFULL-5454] move views folder into frontend folder

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var proxy = require('./backend/routes/updates'),
 
 var app = exports.modules = express();
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/frontend/views');
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'frontend')));
 
