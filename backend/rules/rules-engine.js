c01fc4b5fc33c4212c4691b48ac38520dc54b154
Erwan Queffélec <equeffelec@linagora.com>
2013-10-17 14:27:59 +0200

    OBMFULL-5509 Ensure defaults rules are stored on Rule Engine init

--- backend/rules/rules-engine.js
+++ backend/rules/rules-engine.js
@@ -1,9 +1,60 @@
 'use strict';
 
+var defaultRules = require('./rule-fixtures'),
+    DbProvider = require('../../../backend/mongo-provider'),
+    RulesStorage = require('./rules-storage'),
+    async = require('async');
+
 var RulesEngine = function() {}
 
+RulesEngine.init = function (callbackAllFinished) {
+  var rules = defaultRules.list;
+  var db = DbProvider.db();
+  var manager = new RulesStorage(db);
+  var operations = [];
+
+  for (rule in rules) {
+    operations.push(
+
+      function (callback) {
+
+        RulesStorage.findByCondition(rule.condition, function(err, results){
+
+          if (err) {
+
+            callback(err, null);
+
+          } else {
+
+            if (!results.length) {
+              manager.save(rule, function(err, result) {
+                if (err){
+                  callback(err, null);
+                } else {
+                  callback(null, result);
+                }
+              });
+            } else{
+              callback(null, null);
+            }
+
+          }
+
+        });
+      }
+
+    );
+  }
+
+  async.series(operations, callbackAllFinished);
+};
+
 RulesEngine.prototype.listActions = function () {};
-RulesEngine.prototype.findByPredicate = function (predicate, callback) {};
+
+RulesEngine.prototype.findByPredicate = function (predicate, callback) {
+
+};
+
 RulesEngine.prototype.create = function (rule, callback) {};
 RulesEngine.prototype.update = function (id, rule, callback) {};
 RulesEngine.prototype.delete = function (id, callback) {};
---
+++ test/unit-backend/rules/rules-engine-test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var chai = require('chai');
+
+var expect = chai.expect;
+
+describe('The Rules Engine module', function() {
+
+  it('should add default rules in storage', function(done) {
+    expect(1).to.equal(1);
+    done();
+  });
+
+});
