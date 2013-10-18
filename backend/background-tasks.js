50c5af0b5bf36b8ddaa29631d2d348a17f3b52ac
Michael Bailly <mbailly@linagora.com>
2013-10-18 17:47:31 +0200

    [MU-3] mupee background jobs module

---
+++ backend/background-tasks.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var scraper = require("./mozilla-updates-server-scraper"),
+    scheduler = require("./job-scheduler");
+
+var buildHash = function(clientVersion) {
+  return clientVersion.buildUrl("");
+};
+
+exports.addProductScraperTask = function(clientVersion) {
+  scheduler.addJob(buildHash(clientVersion), function(callback) {
+    scraper(clientVersion, callback);
+  });
+};
