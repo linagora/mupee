aa4842998209f4e1ad5a3cf5238d93d81f88c08d
Erwan Queffélec <equeffelec@linagora.com>
2013-10-30 12:08:07 +0100

    MU-11 Leverage mozilla-version-comparator in latest* actions

--- backend/rules/actions/filter-latest-for-branch.js
+++ backend/rules/actions/filter-latest-for-branch.js
@@ -1,19 +1,6 @@
 'use strict'
 
-function versionsCompare(a, b) {
-  var i, cmp, len, re = /(\.0)+[^\.]*$/;
-
-  a = (a + '').replace(re, '').split('.');
-  b = (b + '').replace(re, '').split('.');
-  len = Math.min(a.length, b.length);
-  for (i = 0; i < len; i++) {
-    cmp = parseInt(a[i], 10) - parseInt(b[i], 10);
-    if (cmp !== 0) {
-      return cmp;
-    }
-  }
-  return a.length - b.length;
-};
+var versionsCompare = require('mozilla-version-comparator');
 
 module.exports = function(branch, candidate) {
   var filtered = [];
