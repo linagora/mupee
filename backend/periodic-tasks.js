df13dc15a2fb6d5bd219cef16c7523c6d7f7568d
Michael Bailly <mbailly@linagora.com>
2013-10-30 17:00:17 +0100

    [MU-3] add periodic-tasks launcher

---
+++ backend/periodic-tasks.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var backgroundTasks = require('./background-tasks');
+var config = require('./config');
+var defaultRefreshIntervalHours = 24;
+
+var intervalId = null;
+
+exports = module.exports = {
+  start: function() {
+    if ( intervalId ) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+    
+    var interval = parseInt(config.fetch.refreshInterval,10);
+    if ( isNaN(interval) ) {
+      interval = defaultRefreshIntervalHours;
+    }
+    interval = interval * 60 * 60 * 1000;
+    
+    var intervalId = setInterval(function() {
+      backgroundTasks.refreshProductUpdates();
+    }, interval);
+  }
+};
---
+++ test/unit-backend/periodic-tasks-test.js
@@ -0,0 +1,62 @@
+/* global setInterval: true */
+
+'use strict';
+
+var mockery = require('mockery'),
+    testLogger = require('./test-logger'),
+    expect = require('chai').expect;
+
+
+describe('The Periodic Tasks module', function() {
+  var periodicTasks;
+  var realSetInterval = setInterval;
+  var config = {
+    fetch: {
+      refreshInterval: 3
+    }
+  };
+
+  beforeEach(function() {
+    mockery.enable({warnOnUnregistered: false});
+    mockery.registerMock('./logger', testLogger);
+    mockery.registerMock('./config', config);
+    periodicTasks = require('../../backend/periodic-tasks');
+  });
+
+  it('should set an interval', function() {
+    setInterval = function(callback, time) {
+      expect(callback).to.be.a.function;
+      expect(time).to.be.a.number;
+      setInterval = realSetInterval;
+    };
+    periodicTasks.start();
+  });
+
+  it('should take the interval from the configuration file', function() {
+    setInterval = function(callback, time) {
+      expect(callback).to.be.a.function;
+      expect(time).to.be.a.number;
+      expect(time).to.equal(3 * 60 * 60 * 1000);
+      setInterval = realSetInterval;
+    };
+    periodicTasks.start();
+  });
+
+  it('should use a default of 24 (hours) when the configuration is bad', function() {
+    setInterval = function(callback, time) {
+      expect(callback).to.be.a.function;
+      expect(time).to.be.a.number;
+      expect(time).to.equal(24 * 60 * 60 * 1000);
+      setInterval = realSetInterval;
+    };
+    config.fetch.refreshInterval = 'not a number !';
+    periodicTasks.start();
+  });
+
+
+  afterEach(function() {
+    mockery.deregisterAll();
+    mockery.resetCache();
+    mockery.disable();
+  });
+});
