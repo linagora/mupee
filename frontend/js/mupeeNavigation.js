2d987450e357474330db0ed0cec4fed320e0c633
jeremie kornobis <jkornobis@linagora.com>
2013-11-07 11:54:40 +0100

    MU-12 Mupee Extension Upload Button, product logo on background

--- frontend/js/mupeeNavigation.js
+++ frontend/js/mupeeNavigation.js
@@ -1,8 +1,16 @@
 'use strict';
 
+function setBackgroundToBody(path) {
+  $(document.body).removeClass();
+  if (path === 'Thunderbird' || path === 'Firefox') {
+    $(document.body).addClass(path);
+  }
+}
+
 angular.module('mupeeNavigation', [])
 .controller('isHomepage', ['$scope', '$location', function($scope, $location) {
   $scope.$on('$routeChangeSuccess', function() {
     $scope.homepage = $location.path() === '/home';
+    setBackgroundToBody($location.path().substr(1));
   });
 }]);
